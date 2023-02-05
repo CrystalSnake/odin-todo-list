4710b2661e0d49a62cb31a911da2518fc18211c1
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2023-02-05 19:59:54 +0400

    Delete task EventListener

--- src/index.js
+++ src/index.js
@@ -3,16 +3,28 @@ import modalContainer from './modal';
 
 const toDoList = [
   {
-    title: 'asdfads',
-    description: 'fsdfasdfasdf',
+    title: 'Task 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
     project: undefined,
     date: '2023-02-10',
   },
-  { title: 'lliosv', description: 'sylkaszxcdf', project: 'Test Project' },
-  { title: 'isfosv', description: 'shylksfdaszxcdf', project: 'Test Project2' },
   {
-    title: 'lliosv',
-    description: 'shkaszxcdf',
+    title: 'Task 2',
+    description:
+      'Luctus accumsan tortor posuere ac ut. Ultricies mi quis hendrerit dolor magna eget est lorem.',
+    project: 'Test Project',
+  },
+  {
+    title: 'Test',
+    description:
+      'Feugiat nisl pretium fusce id velit ut tortor pretium. Ac turpis egestas sed tempus urna.',
+    project: 'My Project',
+  },
+  {
+    title: 'Test task',
+    description:
+      'Magna fermentum iaculis eu non. Volutpat est velit egestas dui.',
     project: 'Test Project',
     date: '2023-02-17',
   },
@@ -212,6 +224,11 @@ function getTask(title, id, date) {
   const taskDelete = document.createElement('button');
   taskDelete.classList.add('pb-1', 'text-rose-700', 'text-2xl');
   taskDelete.textContent = '×';
+  taskDelete.addEventListener('click', () => {
+    toDoList.splice(task.dataset.taskId, 1);
+    renderProjectsList();
+    renderTasks();
+  });
   task.appendChild(taskInfoContainer);
   task.appendChild(taskDelete);
   return task;
